317dc4c02ba92ed46504845432947d706e101d05
Varun Negi <varun.py10@gmail.com>
2023-05-23 21:00:37 +0530

    college-forum

---
+++ forum-frontend/src/app/navbar/sidebar.js
@@ -0,0 +1,10 @@
+export default function Sidebar() {
+  return <div className="w-1/4 bg-slate-700 h-screen p-5">
+    <div className="flex flex-col">
+      <div className="flex justify-center uppercase font-semibold ">
+        Recent threads
+      </div>
+
+    </div>
+  </div>;
+}
